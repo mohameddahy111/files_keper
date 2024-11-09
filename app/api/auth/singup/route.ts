2b65fa03c5d2677c35060ac7aa655210d71aaff5
mohameddahy <mohameddahy111@gmail.com>
2024-11-10 00:26:13 +0200

    fixed

--- app/api/auth/singup/route.ts
+++ app/api/auth/singup/route.ts
@@ -2,6 +2,5 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  console.log(body);
   return NextResponse.json({ message: "Hello, world!" });
 }
